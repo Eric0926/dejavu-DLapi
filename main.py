9a4b1d06c5ba7d3d409da5de7ab6ca9134bd2136
Eric0926 <38851446+Eric0926@users.noreply.github.com>
2020-07-21 00:14:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from search import *
 
 app = Flask(__name__)
@@ -8,8 +8,9 @@ app = Flask(__name__)
 def api_predict():
     images = vgg_search(
         'https://storage.googleapis.com/flagged_evaluation_images/444_10_r2.png', 10)
-    for image in images:
-        print(image)
+    # for image in images:
+    #    print(image)
+    return jsonify(images)
 
 
 if __name__ == "__main__":

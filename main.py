9313490706c9fdaaac984e03d01993157b75e9d7
Eric0926 <38851446+Eric0926@users.noreply.github.com>
2020-07-21 00:06:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from flask import Flask, request
+from search import *
 
 app = Flask(__name__)
+
+
+@app.route("/api_predict")
+def api_predict():
+    images = vgg_search(
+        'https://storage.googleapis.com/flagged_evaluation_images/444_10_r2.png', 10)
+    for image in images:
+        print(image)

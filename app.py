debe80f390388e97e3e24b9fefaa38ec6567310e
Eric0926 <38851446+Eric0926@users.noreply.github.com>
2020-07-22 19:05:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,16 @@ def search():
     results_num = int(request.form["results_num"])
     try:
         # images = vgg_search('https://storage.googleapis.com/flagged_evaluation_images/444_10_r2.png', 10)
+        print(img_url)
+        print(results_num)
         results = vgg_search(img_url, results_num)
     except:
         return "invalid image url", 400
-    return jsonify(images), 200
+    return jsonify(results), 200
 
 
 if __name__ == "__main__":
-    os.environ["LD_PRELOAD"] = "~/anaconda3/lib/libmkl_core.so:~/anaconda3/lib/libmkl_sequential.so"
+    # os.environ["LD_PRELOAD"] = "~/anaconda3/lib/libmkl_core.so:~/anaconda3/lib/libmkl_sequential.so"
     # export LD_PRELOAD=~/anaconda3/lib/libmkl_core.so:~/anaconda3/lib/libmkl_sequential.so
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS
     app.run(host='0.0.0.0', port=5000)
